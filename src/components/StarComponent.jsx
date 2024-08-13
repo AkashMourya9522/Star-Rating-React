56873649a5f88ac2ff6d5469488fce25151df669
Akash Mourya <akashmourya9522@gmail.com>
2024-08-13 20:25:54 +0530

    final-commit

--- src/components/StarComponent.jsx
+++ src/components/StarComponent.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 import { CiStar } from "react-icons/ci";
 
-export default function StarComponent({props}){
+export default function StarComponent({ props }) {
+  const [select, setSelect] = useState(0);
+  const [move, setMove] = useState(0);
 
-    const [select,setSelect] = useState(0)
-    const [move,setMove] = useState(0)
-
-    function selectStars(id){
-        console.log('for selection '+id)
-        setSelect(id)
+  function selectStars(id) {
+    if (select === id) {
+      setSelect(0);
+    } else {
+      setSelect(id);
     }
+  }
 
-    function moveFunction(id){
-        console.log("this is for hovering "+id)
-        setMove(id)
-    }
-    function resetMove(){
-        setMove(0)
-    }
+  function moveFunction(id) {
+    setMove(id);
+  }
+  function resetMove() {
+    setMove(0);
+  }
 
-    return <div>
-        {
-            [...Array(props)].map((_,i)=> 
-                <CiStar key={i+1}
-             className={`text-4xl inline ${(i+1)<= (select) || (move) ? `text-orange-500`: `bg-white`}`}
-             onClick={()=>selectStars(i+1)}
-             onMouseMove={()=>moveFunction(i+1)}
-             onMouseLeave={()=>resetMove()}
-             />
-            )
-        }
+  return (
+    <div>
+      {[...Array(props)].map((_, i) => (
+        <CiStar
+          key={i + 1}
+          className={`text-4xl inline ${
+            i + 1 <= (move || select) ? `text-orange-500` : `bg-white`
+          }`}
+          onClick={() => selectStars(i + 1)}
+          onMouseMove={() => moveFunction(i + 1)}
+          onMouseLeave={() => resetMove()}
+        />
+      ))}
     </div>
+  );
 }
